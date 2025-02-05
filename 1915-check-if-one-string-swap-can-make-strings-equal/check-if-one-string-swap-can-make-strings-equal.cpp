fc00d3eb3625f978bfda82170d8e4ed1406d6cd1
Srikar <128158136+Srikar4510@users.noreply.github.com>
2025-02-05 18:46:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (85.97%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,11 +1,39 @@
 class Solution {
 public:
+// //O(N),O(1)
+//     bool areAlmostEqual(string s1, string s2) {
+//         if(s1==s2) return true;
+//         int s1FreqMap[26]={0};
+//         int s2FreqMap[26]={0};
+//         int numdiff =0;
+//         for(int i=0;i<s1.size();i++){
+//            char s1char = s1[i];
+//            char s2char = s2[i];
+
+//            if(s1char != s2char){
+//             numdiff++;
+
+//            if(numdiff>2){
+//                 return false;
+//             }
+//            }
+
+//            s1FreqMap[s1char -'a']++;
+//            s2FreqMap[s2char-'a']++;
+//         }  
+
+//         for(int i=0;i<26;i++){
+//             if(s1FreqMap[i]!=s2FreqMap[i]){
+//                 return false;
+//             }
+//         }
+//     return true;
+
 //O(N),O(1)
     bool areAlmostEqual(string s1, string s2) {
-        if(s1==s2) return true;
-        int s1FreqMap[26]={0};
-        int s2FreqMap[26]={0};
+       
         int numdiff =0;
+        int firstdiffindex =0,secdiffindex=0;
         for(int i=0;i<s1.size();i++){
            char s1char = s1[i];
            char s2char = s2[i];
@@ -16,17 +44,14 @@ public:
            if(numdiff>2){
                 return false;
             }
-           }
-
-           s1FreqMap[s1char -'a']++;
-           s2FreqMap[s2char-'a']++;
-        }  
-
-        for(int i=0;i<26;i++){
-            if(s1FreqMap[i]!=s2FreqMap[i]){
-                return false;
+            else if(numdiff==1){
+                firstdiffindex=i;
             }
-        }
-    return true;
+            else{
+                secdiffindex=i;
+            }
+           }
+        }  
+    return s1[firstdiffindex]==s2[secdiffindex] && s1[secdiffindex]==s2[firstdiffindex];
     }
 };
