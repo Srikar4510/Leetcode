72b63e83ea31a7f9ff962b4bef0d3694e0228aed
Srikar <128158136+Srikar4510@users.noreply.github.com>
2025-02-11 18:15:42 +0530

    Time: 15 ms (5.85%) | Memory: 26.4 MB (5.32%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,11 +1,46 @@
 class Solution {
+// public:
+// //O(n^2),O(n)
+//     string removeOccurrences(string s, string part) {
+//         while(s.find(part)!= string::npos){
+//             int partStartIndex = s.find(part);
+//             s = s.substr(0,partStartIndex) + s.substr(partStartIndex + part.size());
+//         }
+//         return s;
+//     }
 public:
-//O(n^2),O(n)
+//O(n.m),O(n+m)
     string removeOccurrences(string s, string part) {
-        while(s.find(part)!= string::npos){
-            int partStartIndex = s.find(part);
-            s = s.substr(0,partStartIndex) + s.substr(partStartIndex + part.size());
+        stack<char> stk;
+        
+        int strLength = s.size();
+        int partLength = part.size();
+        for(int i=0;i<strLength;i++){
+            stk.push(s[i]);
+            if(stk.size()>=partLength && checkPart(stk,part,partLength)){
+                for(int j=0;j<partLength;j++){
+                    stk.pop();
+                }
+            }
         }
-        return s;
+      string result = "";
+        while (!stk.empty()) {
+            result = stk.top() + result;
+            stk.pop();
+        }
+
+        return result;
     }
+private:
+  bool checkPart(stack<char> &stk,string &part,int partLength){
+    stack<char> tmp =stk;
+    for(int i=partLength-1;i>=0;i--){
+        if(tmp.top()!=part[i]){
+            return false;
+        }
+        tmp.pop();
+    }
+    return true;
+  }
+       
 };
