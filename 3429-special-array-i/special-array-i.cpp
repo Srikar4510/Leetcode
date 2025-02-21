4102caec52f732da6fc9481e14ae731b4e9c673c
Srikar <128158136+Srikar4510@users.noreply.github.com>
2025-02-21 15:10:09 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (22.73%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -3,11 +3,9 @@ public:
     bool isArraySpecial(vector<int>& nums) {
         int n = nums.size();
         if(n==1) return true;
-        int ctr =0;
         for(int i=0;i<n-1;i++){
-            if((nums[i]%2==0&&nums[i+1]%2==0)||(nums[i]%2==1&&nums[i+1]%2==1)){
-                ctr++;
-                if(ctr==1) return false;
+            if(nums[i]%2==nums[i+1]%2){
+                 return false;
             }
         }
         return true;
