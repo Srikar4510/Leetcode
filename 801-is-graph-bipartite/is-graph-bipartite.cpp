276b344160e3769a604095e37234a940b7e7f224
Srikar <128158136+Srikar4510@users.noreply.github.com>
2025-02-26 18:47:30 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (76.32%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool dfs(int node,int col,vector<vector<int>>& graph,vector<int>& color){
+        //color the node 
         color[node]=col;
+        //change the to color the adjacent Nodes
         col = 1-col;
 
         for(auto adjNode:graph[node]){
+            //if adjNode is not colored,go and color them   
             if(color[adjNode]==2){
                 if(!dfs(adjNode,col,graph,color))
                  return false;
             }
+            //if the adjNode is colored and adjNode color is no different return false 
             else if(color[adjNode]!=col){
                 return false;
             }
