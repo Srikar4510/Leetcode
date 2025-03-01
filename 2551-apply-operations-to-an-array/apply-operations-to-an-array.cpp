5eef7354ca9c676017989774940dd473cc3febd6
Srikar <128158136+Srikar4510@users.noreply.github.com>
2025-03-01 13:24:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (62.06%) - LeetSync

---
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> applyOperations(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0;i<n-1;i++){
+            if(nums[i]==nums[i+1]){
+                nums[i] *= 2;
+                nums[i+1]=0;
+            }
+        }
+
+        int nonZeroIndex=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]!=0){
+                nums[nonZeroIndex]=nums[i];
+                nonZeroIndex++;
+
+            }
+        }
+
+        while(nonZeroIndex<n){
+            nums[nonZeroIndex]=0;
+            nonZeroIndex++;
+        }
+        return nums;
+    }
+};
