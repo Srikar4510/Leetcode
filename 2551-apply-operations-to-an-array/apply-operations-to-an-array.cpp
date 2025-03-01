e5a0109183c76405a9d8b694318a150f8af41ebc
Srikar <128158136+Srikar4510@users.noreply.github.com>
2025-03-01 13:25:58 +0530

    Time: 2 ms (12.65%) | Memory: 13.1 MB (62.06%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -8,7 +8,7 @@ public:
                 nums[i+1]=0;
             }
         }
-
+        //traverse through array and bring all non zero elements to before 
         int nonZeroIndex=0;
         for(int i=0;i<n;i++){
             if(nums[i]!=0){
@@ -17,7 +17,8 @@ public:
 
             }
         }
-
+        //after all non zero elements are placed correctly 
+        //make remaining elements of array zero
         while(nonZeroIndex<n){
             nums[nonZeroIndex]=0;
             nonZeroIndex++;
