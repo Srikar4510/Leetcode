06424d0c8864dd6b47323b8aacf93e038b0d4e2c
Srikar <128158136+Srikar4510@users.noreply.github.com>
2025-03-21 10:58:50 +0530

    Time: 35 ms (95.02%) | Memory: 107.6 MB (76.99%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -2,18 +2,22 @@ class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
         
+        // Step 1: Store available supplies in a set
         unordered_set<string> avaiableSupplies(supplies.begin(),supplies.end());
 
+        // Step 2: Create mapping from recipe name to index
         int n = recipes.size();
         unordered_map<string,int> recipeToIndex;
         for(int i=0;i<n;i++){
             recipeToIndex[recipes[i]]=i;
         }
 
+        // Step 3: Create an adjacency list and in-degree array
         unordered_map<string,vector<string>> adj;//ingridient to recipe
         vector<int> indegree(n,0);
         for(int i=0;i<n;i++){
             for(auto& ingredient:ingredients[i]){
+                // If ingredient is not available, it becomes a dependency
                 if(avaiableSupplies.find(ingredient)==avaiableSupplies.end()){
                     adj[ingredient].push_back(recipes[i]);
                     indegree[i]++;
@@ -35,8 +39,10 @@ public:
             string recipe = recipes[node];
             ans.push_back(recipe);
 
+            // If no recipe depends on this one, continue
             if(adj.count(recipe)==0) continue;
-
+            
+            // Reduce in-degree for dependent recipes
             for(auto& dependentRecipe:adj[recipe]){
                 int idx = recipeToIndex[dependentRecipe];
                 indegree[idx]--;
