b96139986e906844685cdd8c937332905ba0dd90
Srikar <128158136+Srikar4510@users.noreply.github.com>
2025-03-27 13:42:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (57.53%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int n = s.size();
+        int i=0,sign=1;
+        long ans = 0;
+
+        //ignore leading white spaces
+        while(i<n && s[i]==' '){
+            i++;
+        }
+
+        //check for sign and move to nect index
+        if(s[i]=='-'){
+            sign=-1;
+            i++;
+        }
+        else if(s[i]=='+'){
+            i++;
+        }
+
+        //iterate over remaining string
+        while(i<n){
+            //is a digit
+            if(s[i]>='0'&&s[i]<='9'){
+                  ans = ans*10 + (s[i]-'0');
+
+                  if(ans*sign > INT_MAX) return INT_MAX;
+                  if(ans*sign < INT_MIN) return INT_MIN;
+                  i++;
+            }
+            else{
+                 break;
+            }
+        }
+       return (int)(ans*sign);
+    }
+};
