5ff7f0bba88fbeb70b6986ac9cdc6e3f9d9b7d23
Srikar <128158136+Srikar4510@users.noreply.github.com>
2025-04-30 10:00:25 +0530

    Time: 4 ms (4.46%) | Memory: 14.5 MB (11.46%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    int countDigits(int num){
-        int cnt =0;
-        while(num){
-            num = num/10;
-            cnt++;
-        }
-        return cnt;
-    }
-    int findNumbers(vector<int>& nums) {
-        int ans =0,n=nums.size();
-        for(int i=0;i<n;i++){
-            if(countDigits(nums[i]) % 2==0){
+    // int countDigits(int num){
+    //     int cnt =0;
+    //     while(num){
+    //         num = num/10;
+    //         cnt++;
+    //     }
+    //     return cnt;
+    // }
+    // int findNumbers(vector<int>& nums) {
+    //     int ans =0,n=nums.size();
+    //     for(int i=0;i<n;i++){
+    //         if(countDigits(nums[i]) % 2==0){
+    //             ans++;
+    //         }
+    //     }
+    //     return ans;
+    // }
+
+       int findNumbers(vector<int>& nums) {
+           int ans = 0,n = nums.size();
+           for(int i=0;i<n;i++){
+             int length = to_string(nums[i]).length();
+
+             if(length % 2 ==0){
                 ans++;
-            }
-        }
-        return ans;
+             }
+           }
+           return ans;
     }
 };
